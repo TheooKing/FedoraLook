57819eec1eec83e71ef2aa4c1e7df3d4f5cf0568
Theolaos <92591390+theolaos@users.noreply.github.com>
2024-04-28 13:33:51 +0300

    Create kde.sh

---
+++ kde.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Lazy to make the script
+
+# colloid icons
+
+# using kvantum - utterly nord solid
+# nordic cursors
+
+# general font Monospace with Noto Sans
+
+# rely on a video on how to customize kde
