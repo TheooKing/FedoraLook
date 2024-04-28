c8b121f2bf1667f93b1a5c5f40aff0d39ae55895
Theolaos <92591390+theolaos@users.noreply.github.com>
2024-04-28 13:41:19 +0300

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Lazy to make the script
+
+echo "Lazy to make the script..."
 
 # colloid icons
 
@@ -10,3 +11,34 @@
 # general font Monospace with Noto Sans
 
 # rely on a video on how to customize kde
+
+# - Git repos
+# auto-cpu freq (theolaos star)
+# spotify ad-block BashSpotX
+# kde rounded corners (theolaos star)
+
+# - general applications
+# nvim
+# vscode
+# netbeans configured for C++
+# Discord (flatpak)
+# spotify (flatpak)
+# obsidian
+# pixelorama
+# vivaldi + brave
+
+# - development
+# CMake
+# GCC
+# libglfw
+# c++ devels (?)
+# pip
+# pip pygame-ce
+# pip pymunk
+# pip moderngl
+# pip PyOpenGL
+# pip numpy
+# pip pyinstaller
+
+# - virual machines
+# setup a windows 10 env with 12gb ram and 100gb space
